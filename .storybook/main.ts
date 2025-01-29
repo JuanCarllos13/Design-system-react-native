a288c1b184927002679270e6c133100c534302a8
Juan Carlos <juan2017amaral@gmail.com>
2025-01-28 20:32:29 -0400

    first commit

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/react-native-web-vite').StorybookConfig } */
+const config = {
+  stories: [
+    '../src/stories/**/*.mdx',
+    '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-native-web-vite',
+    options: {},
+  },
+};
+export default config;
