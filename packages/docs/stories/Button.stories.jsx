b2eceecd296e118699c62e856bdc9226f2dee75a
Juan Carlos <juan2017amaral@gmail.com>
2025-02-09 14:36:07 -0400

    fix: create storybook

---
+++ packages/docs/stories/Button.stories.jsx
@@ -0,0 +1,70 @@
+
+import { fn } from '@storybook/test';
+import React from 'react';
+import { View } from 'react-native';
+import { Button } from 'design-system-react-native';
+import { ArrowRight, ArrowLeft } from 'phosphor-react-native';
+
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <View>
+        <Story />
+      </View>
+    ),
+  ],
+  args: { onPress: fn() },
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    label: 'Primary Button',
+    children: (
+      <Button>
+        <Button.Title>Entrar</Button.Title>
+      </Button>
+    ),
+    args: { onPress: fn() },
+  },
+};
+
+export const Loading = {
+  args: {
+    isLoading: true,
+    label: 'Loading...',
+  },
+  children: (
+    <Button isLoading>
+      <Button.Title>Entrar</Button.Title>
+    </Button>
+  ),
+};
+
+export const IconButtonLeft = {
+  args: {
+    label: 'Button with Icon on the left',
+    children: (
+      <Button>
+        <Button.Icon icon={ArrowLeft} />
+        <Button.Title>Entrar</Button.Title>
+      </Button>
+    ),
+  },
+};
+
+export const IconButtonRight = {
+  args: {
+    label: 'Button with Icon on the right',
+    children: (
+      <Button>
+        <Button.Title>Entrar</Button.Title>
+        <Button.Icon icon={ArrowRight} />
+      </Button>
+    ),
+  },
+};
---
+++ packages/docs/stories/ButtonAnimated.stories.jsx
@@ -0,0 +1,25 @@
+import { fn } from '@storybook/test';
+import { View } from 'react-native';
+import { AnimatedButton } from 'design-system-react-native';
+
+const meta = {
+  title: 'Example/AnimatedButton',
+  component: AnimatedButton,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <View style={{ padding: 20, alignItems: 'center' }}>
+        <Story />
+      </View>
+    ),
+  ],
+  args: { onPress: fn() },
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    title: 'Enviar',
+  },
+};
